a3dab51d7c9561c0126376c5447613563e2bcd1c
Nandira Paturohman <nandirasp@gmail.com>
2017-10-18 13:58:50 +0700

    tictactoe Done

---
+++ tic-tac-toe.js
@@ -0,0 +1,35 @@
+function ticTacToe(){
+  let mark = ['X','O'];
+  let countX = 0;
+  let countO = 0;
+  let result = [];
+  for (let i = 0; i < 3; i++) {
+    let row = [];
+    for (let j = 0; j < 3; j++) {
+      let random = Math.floor(Math.random(0)*mark.length);
+      if(mark[random] == 'X'){
+        if (countX < 5) {
+          row.push(mark[random]);
+          countX++
+        }else{
+          row.push('O');
+          countO++
+        }
+      }else if(mark[random] == 'O'){
+        if (countO < 5) {
+          row.push(mark[random]);
+          countO++
+        }else{
+          row.push('X');
+          countX++
+        }
+      }
+    }
+    result.push(row);
+  }
+  console.log(countX);
+  console.log(countO);
+  return result;
+}
+
+console.log(ticTacToe());
